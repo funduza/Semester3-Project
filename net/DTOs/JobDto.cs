efad33ebc9b1df77fbd0c4e95deabe6f560b147a
Eser Sarıyar <esersariyar@gmail.com>
2024-11-20 15:51:48 +0100

    add jobs controller

---
+++ net/DTOs/JobDto.cs
@@ -0,0 +1,7 @@
+﻿namespace DTOs;
+
+public class JobDto
+{
+    public required long Id { get; set; }
+    public required string Title { get; set; }
+}
---
+++ net/Server/Controllers/JobsController.cs
@@ -0,0 +1,29 @@
+﻿using DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Server.Grpc;
+
+namespace Server.Controllers;
+
+[ApiController]
+[Route("jobs")]
+public class JobsController : ControllerBase
+{
+    private readonly Data.DataClient _dataClient;
+
+    public JobsController(Data.DataClient dataClient)
+    {
+        _dataClient = dataClient;
+    }
+
+    [HttpGet]
+    public async Task<IResult> GetAllJobs()
+    {
+        var getAllJobsResponse = await _dataClient.getAllJobsAsync(new GetAllJobsRequest());
+        var jobDtos = getAllJobsResponse.Jobs.Select(job => new JobDto()
+        {
+            Id = job.Id,
+            Title = job.Title,
+        });
+        return Results.Ok(jobDtos);
+    }
+}

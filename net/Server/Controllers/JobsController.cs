2043362df8d34732c87ca22cf5765a896760349e
funduza <buruncukdora@gmail.com>
2024-11-27 14:14:06 +0100

    add search input and a method in controller

--- net/Server/Controllers/JobsController.cs
+++ net/Server/Controllers/JobsController.cs
@@ -33,4 +33,27 @@ public class JobsController : ControllerBase
         
         return Ok(jobsDto);
     }
+    
+    [HttpGet("search")] //added search path dont know if we want it
+    public async Task<ActionResult<IEnumerable<JobDto>>> SearchJobs([FromQuery] string query)
+    {
+        if (string.IsNullOrWhiteSpace(query))
+        {
+            return BadRequest("search query empty");
+        }
+
+        var response = await _dataClient.getSearchedJobsAsync(new GetSearchedJobsRequest { Query = query });
+        var jobsDto = response.Jobs.Select(job => new JobDto
+        {
+            Id = job.Id,
+            Title = job.Title,
+            Description = job.Description,
+            Salary = job.Salary,
+            Type = job.Type,
+            Deadline = job.Deadline,
+            Location = job.Location
+        });
+
+        return Ok(jobsDto);
+    }
 }

b6b18820a115dbc599b7cd214806f6d626ac9324
Eser Sarıyar <35234218+esersariyar@users.noreply.github.com>
2024-11-29 14:15:47 +0100

    Merge pull request #5
    * Create SearchJobAscync
    
    * Merge branch 'main' into Eser/Search
    
    * Merge remote-tracking branch 'origin/Blazor_job' into Eser/Search

--- net/Client/Service/HttpJobService.cs
+++ net/Client/Service/HttpJobService.cs
@@ -4,17 +4,29 @@ namespace Client.Service;
 
 public class HttpJobService(HttpClient httpClient) : IJobService
 {
-
     public async Task<List<JobDto>> GetAllJobsAsync()
     {
         HttpResponseMessage response = await httpClient.GetAsync("jobs");
 
         if (!response.IsSuccessStatusCode)
         {
-            throw new Exception("Failed to retrieve posts.");
+            throw new Exception("Failed to retrieve jobs.");
         }
 
-        List<JobDto>? posts = await response.Content.ReadFromJsonAsync<List<JobDto>>();
-        return posts ?? new List<JobDto>();
+        List<JobDto>? jobs = await response.Content.ReadFromJsonAsync<List<JobDto>>();
+        return jobs ?? new List<JobDto>();
+    }
+
+    public async Task<List<JobDto>> SearchJobAsync(string searchTerm)
+    {
+        HttpResponseMessage response = await httpClient.GetAsync($"jobs/search?query={Uri.EscapeDataString(searchTerm)}");
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception("Failed to search jobs.");
+        }
+
+        List<JobDto>? jobs = await response.Content.ReadFromJsonAsync<List<JobDto>>();
+        return jobs ?? new List<JobDto>();
     }
 }

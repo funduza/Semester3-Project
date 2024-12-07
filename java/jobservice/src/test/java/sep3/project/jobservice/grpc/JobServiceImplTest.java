dcb4a2c6f6224a6923aa986781d2be735ed28a88
Alex Tofan <7130188+savaryna@users.noreply.github.com>
2024-12-07 21:28:23 +0100

    Unit test for viewing a single job (#20)
    * Unit test for viewing a single job
    
    * extract test job creation, verify repository call
    
    ---------
    
    Co-authored-by: supendd <supendradbogati@gmail.com>

--- java/jobservice/src/test/java/sep3/project/jobservice/grpc/JobServiceImplTest.java
+++ java/jobservice/src/test/java/sep3/project/jobservice/grpc/JobServiceImplTest.java
@@ -1,16 +1,22 @@
 package sep3.project.jobservice.grpc;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
 import io.grpc.internal.testing.StreamRecorder;
+import org.mockito.junit.jupiter.MockitoExtension;
+import sep3.project.jobservice.entities.Job;
+import sep3.project.jobservice.entities.JobProvider;
 import sep3.project.jobservice.repositories.JobRepository;
 
+import java.util.*;
+
+@ExtendWith(MockitoExtension.class)
 public class JobServiceImplTest {
     @Mock
     private JobRepository jobRepository;
@@ -18,17 +24,63 @@ public class JobServiceImplTest {
     @InjectMocks
     private JobServiceImpl jobServiceImpl;
 
-    private StreamRecorder<ListJobsResponse> responseObserver;
-
-    @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.openMocks(this);
-        responseObserver = StreamRecorder.create();
+    @Test
+    public void testJobServiceImplInitialization() {
+        assertNotNull(jobServiceImpl);
+        assertNotNull(jobRepository);
     }
 
     @Test
-    public void testJobServiceImplInitialization() throws Exception {
-        assertNotNull(jobServiceImpl);
-        assertNotNull(jobRepository);
+    public void testGetJob() throws Exception {
+        // Arrange
+        Job testJob = getJob();
+        when(jobRepository.findById(testJob.getId())).thenReturn(Optional.of(testJob));
+
+        StreamRecorder<JobProto> response = StreamRecorder.create();
+
+        // Act
+        jobServiceImpl.getJob(GetJobRequest.newBuilder().setId(1).build(), response);
+
+        // Assert
+        assertNull(response.getError());
+        assertEquals(1, response.getValues().size());
+        assertEquals(1L, response.firstValue().get().getId());
+
+        assertThrows(NoSuchElementException.class, () -> jobServiceImpl.getJob(GetJobRequest.newBuilder().setId(2).build(), response));
+        verify(jobRepository, times(2)).findById(anyLong());
+    }
+
+    private JobProvider getJobProvider(long id) {
+        return JobProvider.newBuilder()
+                .setId(id)
+                .setEmail("test@test.test")
+                .setPassword("test")
+                .setName("test")
+                .setDescription("test")
+                .setPhoneNumber("1000")
+                .build();
+    }
+
+    private JobProvider getJobProvider() {
+        return getJobProvider(1L);
+    }
+
+    private Job getJob(long id) {
+        return Job.newBuilder()
+                .setId(id)
+                .setTitle("test")
+                .setDescription("test")
+                .setPostingDate(new Date())
+                .setDeadline(new Date())
+                .setLocation("test")
+                .setType(Job.Type.FullTime)
+                .setSalary(1000.0)
+                .setStatus(Job.Status.Active)
+                .setJobProvider(getJobProvider(id))
+                .build();
+    }
+
+    private Job getJob() {
+        return getJob(1L);
     }
 }

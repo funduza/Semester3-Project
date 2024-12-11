5e1edb1e503d08850efce69996b5868f8d17c404
PawsitiveCoder <167691828+PawsitiveCoder@users.noreply.github.com>
2024-12-11 12:59:35 +0100

    add .http test, correct package name

--- java/jobservice/src/main/java/sep3/project/jobservice/grpc/JobApplicationServiceImpl.java
+++ java/jobservice/src/main/java/sep3/project/jobservice/grpc/JobApplicationServiceImpl.java
@@ -3,28 +3,25 @@ package sep3.project.jobservice.grpc;
 import io.grpc.stub.StreamObserver;
 import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
-import sep3.project.applicationservice.grpc.CreateJobApplicationRequest;
-import sep3.project.applicationservice.grpc.JobApplicationProto;
-import sep3.project.applicationservice.grpc.JobApplicationServiceGrpc;
 import sep3.project.jobservice.entities.Job;
 import sep3.project.jobservice.entities.JobApplication;
 import sep3.project.jobservice.entities.JobSeeker;
 import sep3.project.jobservice.repositories.JobApplicationRepository;
-import sep3.project.jobservice.repositories.JobProviderRepository;
 import sep3.project.jobservice.repositories.JobRepository;
 import sep3.project.jobservice.repositories.JobSeekerRepository;
 
 @Slf4j
 @GrpcService
 public class JobApplicationServiceImpl extends JobApplicationServiceGrpc.JobApplicationServiceImplBase {
-
     private final JobApplicationRepository jobApplicationRepository;
-
     private final JobRepository jobRepository;
     private final JobSeekerRepository jobSeekerRepository;
 
-    public JobApplicationServiceImpl(JobApplicationRepository jobApplicationRepository, JobProviderRepository jobProviderRepositor,
-                                     JobRepository jobRepository, JobSeekerRepository jobSeekerRepository) {
+    public JobApplicationServiceImpl(
+            JobApplicationRepository jobApplicationRepository,
+            JobRepository jobRepository,
+            JobSeekerRepository jobSeekerRepository
+    ) {
         this.jobApplicationRepository = jobApplicationRepository;
         this.jobRepository = jobRepository;
         this.jobSeekerRepository = jobSeekerRepository;
@@ -35,28 +32,26 @@ public class JobApplicationServiceImpl extends JobApplicationServiceGrpc.JobAppl
         log.info("CreateJobApplicationRequest: {}", request);
 
         JobApplicationProto jobApplicationProto = request.getJobApplication();
-        JobApplicationProto response = null;
-        JobSeeker jobSeeker = jobSeekerRepository.getReferenceById(jobApplicationProto.getJobSeekerId());
-        Job job = jobRepository.getReferenceById(jobApplicationProto.getJobId());
+        Job job = jobRepository.findById(jobApplicationProto.getJobId()).orElseThrow();
+        JobSeeker jobSeeker = jobSeekerRepository.findById(jobApplicationProto.getJobSeekerId()).orElseThrow();
 
         JobApplication jobApplication = JobApplication.newBuilder()
-                .setJobSeeker(jobSeeker)
-                .setJob(job)
                 .setStatus(JobApplication.Status.InProgress)
+                .setJob(job)
+                .setJobSeeker(jobSeeker)
                 .build();
 
         JobApplication savedJobApplication = jobApplicationRepository.save(jobApplication);
 
-        response = JobApplicationProto.newBuilder()
+        JobApplicationProto response = JobApplicationProto.newBuilder()
                 .setId(savedJobApplication.getId())
-                .setJobSeekerId(savedJobApplication.getJobSeeker().getId())
-                .setJobId(savedJobApplication.getJob().getId())
                 .setStatus(savedJobApplication.getStatus().toString())
                 .setApplicationDate(savedJobApplication.getApplicationDate().toString())
+                .setJobId(savedJobApplication.getJob().getId())
+                .setJobSeekerId(savedJobApplication.getJobSeeker().getId())
                 .build();
 
         responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
 }
-
